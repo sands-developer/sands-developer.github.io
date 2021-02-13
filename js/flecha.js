84103af7f7f297a1ebdd9b2a51067332a5cc756b
jonathan <sands-jt@hotmail.com>
2021-02-12 20:37:06 -0500

    Initial commit

---
+++ js/flecha.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+$(window).scroll(function(){
+    if($(this).scrollTop()>200){
+        $('#topBtn').fadeIn();
+    }else{
+        $('#topBtn').fadeOut();
+    }
+});
+$("#topBtn").click(function(){
+    $('html,body').animate({scrollTop : 0},600);
+});
+});
